857288d61846a27506f281838b7ca417e38f2e5d
OMEN2 <1617218599@qq.com>
2021-05-06 23:27:10 +0800

    set sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,17 @@ module.exports = {
     title: 'Rice Shower',
     description: '一马当先，万马无光',
     themeConfig:{
-        sidebar:'auto',
+        sidebar:{
+            '/fe/':[
+                '/fe/',
+                {
+                    title:'Vue',
+                    children:[
+                        '/fe/关于mounted的bug'
+                    ]
+                }
+            ]
+        },
         nav:[
             {text:'Fe',link:'/Fe/'},
             {text:'GitHub',link:'https://github.com/CruelPineapple'}         
