76b26ee8a5a40ca53f56aeba6f5863db87c11ee4
ssandra <1617218599@qq.com>
2021-12-10 16:35:13 +0800

    mov

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -67,6 +67,7 @@ module.exports = {
           '/Fe/engine/事件循环',
           '/Fe/engine/v8',
           '/Fe/engine/上下文',
+          '/Fe/回流重绘',
           '/Fe/engine/gc'
         ]
       },
@@ -172,8 +173,7 @@ module.exports = {
         title: 'CSS',
         children:[
           '/Fe/css/三列布局',
-          '/Fe/css/display',
-          '/Fe/css/回流重绘'
+          '/Fe/css/display'
         ]
       },
       {
